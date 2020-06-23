98a4d14c54f7e8ad73d7810b3e7c247cb134cc9d
seanmcneely <smcneely@usc.edu>
2020-06-23 16:02:48 -0400

    Add files via upload

---
+++ SiteVersion1/filter.js
@@ -0,0 +1,14 @@
+//Performs filtering of projects based on class name
+
+filterSelection("all")
+
+function filterSelection(c) {
+	recordFilterClick();
+	var x, i;
+	x = document.getElementsByClassName("filterDiv");
+	if (c == "all") c = "";
+	for (i = 0; i < x.length; i++) {
+	   if (x[i].className.indexOf(c) > -1) x[i].style.display = "inline-block";
+	   else x[i].style.display = "none";
+	}
+}
